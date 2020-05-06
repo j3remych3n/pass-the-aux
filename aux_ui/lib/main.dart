ed342c5af5c4cf02c7746344758be8b2c47bf7e6
Jeremy Chen <asdftwinkie@gmail.com>
2020-05-06 00:26:16 -0700

    almost done

--- aux_ui/lib/main.dart
+++ aux_ui/lib/main.dart
@@ -73,13 +73,13 @@ class _GuestRegState extends State<GuestRegName> {
           padding: EdgeInsets.only(top: SizeConfig.notchHeight),
           child: AuxCard(
             borderColor: auxAccent,
-            padding: 30.0,
+            padding: 24.0,
             child: Column(
               mainAxisSize: MainAxisSize.max,
               children: <Widget>[
                 Expanded(
                   child: Padding(
-                    padding: EdgeInsets.only(bottom: 24),
+                    padding: EdgeInsets.only(bottom: 20),
                     child: Align(
                       alignment: Alignment.bottomLeft,
                       child: Text(
@@ -91,13 +91,50 @@ class _GuestRegState extends State<GuestRegName> {
                 ),
                 Expanded(
                   child: Padding(
-                    padding: EdgeInsets.only(top: 24),
+                    padding: EdgeInsets.only(top: 20),
                     child: Align(
                       alignment: Alignment.topCenter,
                       child: Column(
                         children: <Widget>[
-                          Text('placeholder 1'),
-                          Text('placeholder 2'),
+                          TextField(
+                            style: auxAccentButton,
+                            textAlignVertical: TextAlignVertical.bottom,
+                            decoration: InputDecoration(
+                              prefixIcon: Icon(
+                                Icons.short_text, 
+                                color:auxAccent,
+                                size: 26.0,
+                                semanticLabel: "Short text for user nickname",
+                              ),
+                              labelStyle: auxAccentButton,
+                              labelText: 'enter a nickname',
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white, width: 3),
+                                borderRadius: BorderRadius.circular(10), // TODO: scale by screen resolution
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white, width: 3),
+                                borderRadius: BorderRadius.circular(10), // TODO: scale by screen resolution
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(20),
+                            child: Text('or', style: auxAccentButton)
+                          ),
+                          SizedBox(
+                            width: double.infinity,
+                            child: FlatButton.icon(
+                            onPressed:() {},
+                            color: auxAccent,
+                            padding: EdgeInsets.all(15),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10)
+                            ),
+                            icon: new Image.asset('assets/spotify_logo.png', height: 21, width: 21),
+                            label: Text('sign up with spotify')
+                          ),
+                          )
                         ],
                       )
                     )
