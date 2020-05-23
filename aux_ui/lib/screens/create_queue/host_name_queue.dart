ff43dfd2bc2bceb97e08f8aab16c62f152ecff20
Diane Hu <diane.hu@duke.edu>
2020-05-22 20:36:29 -0500

    full refactor

--- aux_ui/lib/screens/create_queue/host_name_queue.dart
+++ aux_ui/lib/screens/create_queue/host_name_queue.dart
@@ -6,14 +6,13 @@ import 'package:aux_ui/widgets/layout/nux_container.dart';
 import 'package:aux_ui/widgets/text_input/aux_text_field.dart';
 
 class HostNameQueue extends SequentialWidget {
-  const HostNameQueue({Key key, String nextPage}) : super(key: key, nextPage: nextPage);
+  const HostNameQueue({Key key, String nextPage})
+      : super(key: key, nextPage: nextPage);
+
   _HostNameQueueState createState() => _HostNameQueueState();
 }
 
 class _HostNameQueueState extends State<HostNameQueue> {
-  bool _initialized = false;
-  Widget _namePromptText;
-  Widget _nameQueueTextField;
   String queueName = "";
   TextEditingController queueNameController;
 
@@ -37,48 +36,37 @@ class _HostNameQueueState extends State<HostNameQueue> {
     queueName = text;
   }
 
-  void _initializeWidgets() {
-    if (_initialized)
-      return; // don't waste time reinitializing widgets on rebuild
-    _initialized = true;
-
-    _namePromptText = Align(
-        alignment: Alignment.bottomLeft,
-        child: Text("one more step and we're ready to roll", style: auxDisp3));
-
-    _nameQueueTextField = Align(
-        alignment: Alignment.center,
-        child: Padding(
-            padding: EdgeInsets.only(bottom: SizeConfig.safeAreaVertical * 1.5),
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  AuxTextField(
-                    controller: queueNameController,
-                    onSubmitted: submitted,
-                    onChanged: changed,
-                    icon: Icon(
-                      Icons.short_text,
-                      color: auxAccent,
-                      size: 26.0,
-                      semanticLabel: "aux queue name",
-                    ),
-                    label: 'name your aux queue',
-                  )
-                ])));
-  }
-
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
-    _initializeWidgets();
-
     return NuxContainer(
         topFlex: 6,
         title: 'aux',
-        topWidget: _namePromptText,
-        bottomWidget: _nameQueueTextField);
+        topWidget: const Align(
+            alignment: Alignment.bottomLeft,
+            child: Text(
+                "one more step and we're ready to roll", style: auxDisp3)),
+        bottomWidget: Align(
+            alignment: Alignment.center,
+            child: Padding(
+                padding: EdgeInsets.only(
+                    bottom: SizeConfig.safeAreaVertical * 1.5),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      AuxTextField(
+                        controller: queueNameController,
+                        onSubmitted: submitted,
+                        onChanged: changed,
+                        icon: Icon(
+                          Icons.short_text,
+                          color: auxAccent,
+                          size: 26.0,
+                          semanticLabel: "aux queue name",
+                        ),
+                        label: 'name your aux queue',
+                      )
+                    ]))));
   }
-
 }
