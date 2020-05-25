58843c574ddc0f6f340cf8e09b9582b92ebc7fda
jeremy <asdftwinkie@gmail.com>
2020-05-25 03:18:19 -0700

    search call working, with some search quality optimization

--- aux_ui/lib/aux_lib/spotify_session.dart
+++ aux_ui/lib/aux_lib/spotify_session.dart
@@ -65,80 +65,47 @@ class SpotifySession {
     return _connected;
   }
 
-
+  // TODO: pipe optional args later
   Future<List<Song>> search(
     String query, 
     { List<SearchType> types = const [SearchType.track], 
       int limit = 20, 
-      String market = 'US'}) async {
-        
-      var results = await _webApi.search
-        .get(query, types);
+      String market = 'US',
+      bool sorted = true }) async {
 
+      Set<Song> resultSet = (await _naiveSearch(query)).toSet();
+      var pickySearch = await _naiveSearch('"${query}"');
+      resultSet.addAll(pickySearch);
+      var results = resultSet.toList();
+      results.sort((a, b) => b.popularity.compareTo(a.popularity));
+
+      return results.sublist(0, limit);
+  }
+
+  Future<List<Song>> _naiveSearch(
+    String query, 
+    { List<SearchType> types = const [SearchType.track], 
+      int limit = 20, 
+      String market = 'US',
+      bool sorted = true }) async {
       
-        
-      /*var search = await spotify.search
-      .get('metallica')
-      .first(2)
+      var rawResults = await _webApi.search
+      .get(query, types)
+      .first(limit)
       .catchError((err) => print((err as SpotifyException).message));
-  if (search == null) {
-    return;
+      
+      return rawResults
+        .map((page) => page.items
+            .where((i) => i is Track)
+            .map((track) => _songFromApi(track)))
+        .expand((i) => i)
+        .toList();
+    }
+
+  Song _songFromApi(Track track) {
+    String artist = track.artists[0].name;
+    String cover = track.album.images[0].url;
+    return new Song(track.name, artist, cover, track.uri.toString(), popularity: track.popularity);
   }
-  search.forEach((pages) {
-    pages.items.forEach((item) {
-      if (item is PlaylistSimple) {
-        print('Playlist: \n'
-            'id: ${item.id}\n'
-            'name: ${item.name}:\n'
-            'collaborative: ${item.collaborative}\n'
-            'href: ${item.href}\n'
-            'trackslink: ${item.tracksLink.href}\n'
-            'owner: ${item.owner}\n'
-            'public: ${item.owner}\n'
-            'snapshotId: ${item.snapshotId}\n'
-            'type: ${item.type}\n'
-            'uri: ${item.uri}\n'
-            'images: ${item.images.length}\n'
-            '-------------------------------');
-      }
-      if (item is Artist) {
-        print('Artist: \n'
-            'id: ${item.id}\n'
-            'name: ${item.name}\n'
-            'href: ${item.href}\n'
-            'type: ${item.type}\n'
-            'uri: ${item.uri}\n'
-            '-------------------------------');
-      }
-      if (item is TrackSimple) {
-        print('Track:\n'
-            'id: ${item.id}\n'
-            'name: ${item.name}\n'
-            'href: ${item.href}\n'
-            'type: ${item.type}\n'
-            'uri: ${item.uri}\n'
-            'isPlayable: ${item.isPlayable}\n'
-            'artists: ${item.artists.length}\n'
-            'availableMarkets: ${item.availableMarkets.length}\n'
-            'discNumber: ${item.discNumber}\n'
-            'trackNumber: ${item.trackNumber}\n'
-            'explicit: ${item.explicit}\n'
-            '-------------------------------');
-      }
-      if (item is AlbumSimple) {
-        print('Album:\n'
-            'id: ${item.id}\n'
-            'name: ${item.name}\n'
-            'href: ${item.href}\n'
-            'type: ${item.type}\n'
-            'uri: ${item.uri}\n'
-            'albumType: ${item.albumType}\n'
-            'artists: ${item.artists.length}\n'
-            'availableMarkets: ${item.availableMarkets.length}\n'
-            'images: ${item.images.length}\n'
-            '-------------------------------');
-      } 
-    });
-  });*/
-  }
+
 }
